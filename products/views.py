e84c8ecb1f7feedd62638d5be0d8ef1e5efbbcf9
Saurabh <saurabhp14344@gmail.com>
2019-07-04 18:44:12 +0530

    Project completed

--- products/views.py
+++ products/views.py
@@ -1,4 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects
+    return render(request, 'home.html' , {'products':products})
+
+@login_required(login_url='/accounts/login')
+def create(request):
+    if request.method=='POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['image'] and request.FILES['icon']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://'+ request.POST['url']
+            product.image = request.FILES['image']
+            product.icon = request.FILES['icon']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/' + str(product.id))
+
+        else:
+            return render(request, 'create.html', {'error': 'All Fields Are Required'})
+    else:
+        return render(request, 'create.html')
+
+def details(request, product_id):
+    prod = get_object_or_404(Product, pk=product_id)
+    return render(request, 'details.html', {'product':prod})
+
+@login_required(login_url='/accounts/login')
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.vote_total +=1
+        product.save()
+        return redirect('home')
